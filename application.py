e50761a6678229ac709d944e2d586e5bdc00d2b2
Nazib <nazibsorathiya@gmail.com>
2021-03-30 02:09:25 -0600

    modified code to handle new format

--- application.py
+++ application.py
@@ -23,16 +23,28 @@ def fulfillment():
 			data_json = json.loads(data)
 
 			query_result = data_json['queryResult']
-			print(query_result)
-			param = query_result['parameters']['lab_test_type']
+			lab_test_type = query_result['parameters']['lab_test_type']
 			intent_name = query_result['intent']['displayName']
-			print(param, intent_name)
+			
+			faq_df = pd.read_excel('intents_and_responses.xlsx', sheet_name = 'Sheet1', engine='openpyxl')
+	
+			faq_df_test_df = faq_df[faq_df['Lab Test Type'] == lab_test_type]
+			faq_df_test_df = faq_df_test_df.reset_index(drop=True)
+			
+			if len(faq_df_test_df) == 0:
+				text_message = constants.NO_LAB_TEST_ERROR + ' ' + lab_test_type
+			else:
+				text_message = faq_df_test_df[intent_name][0]
 
-			faq_df = pd.read_excel('intent_faqs.xlsx', sheet_name = 'Sheet1', engine='openpyxl')
-			faq_intent_df = faq_df[faq_df['Intent'] == intent_name]
-			faq_intent_param_df = faq_intent_df[faq_intent_df['Parameter'] == param]
-			faq_intent_param_df = faq_intent_param_df.reset_index(drop=True)
-			text_message = faq_intent_param_df['Response'][0]
+			if pd.isna(text_message):
+				text_message = constants.NO_DATA_FOR_TEST_INTENT_1 + ' ' + lab_test_type + ' ' + constants.NO_DATA_FOR_TEST_INTENT_2 + ' ' + intent_name
+
+			# faq_df_test_intent_df = faq_df_test_df[faq_df_test_df[intent_name]
+
+			# faq_intent_df = faq_df[faq_df['Intent'] == intent_name]
+			# faq_intent_param_df = faq_intent_df[faq_intent_df['Parameter'] == param]
+			# faq_intent_param_df = faq_intent_param_df.reset_index(drop=True)
+			# text_message = faq_intent_param_df['Response'][0]
 
 		except KeyError:
 			text_message = constants.ERROR_MESSAGE
--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
-ERROR_MESSAGE = 'There was a error in generating the response.'
+ERROR_MESSAGE = 'There was an error in generating the response.'
+NO_LAB_TEST_ERROR = 'No lab test found with the name'
+NO_DATA_FOR_TEST_INTENT_1 = 'No data found for the lab test'
+NO_DATA_FOR_TEST_INTENT_2 = ' and the intent'
 
